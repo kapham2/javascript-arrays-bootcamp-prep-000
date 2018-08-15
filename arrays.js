c850b99afa7c96156d0eed4a78130ae4ecbb547f
Kim Pham <pham.kim.a@gmail.com>
2018-08-15 02:26:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,14 @@ function destructivelyAddElementToBeginningOfArray(array, element)
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element)
+{
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element)
+  return array
+}
